b67124c299109dd9942ceea28e4afe5b3c4f3337
Nao ARGOUSE <nargouse@student.42.fr>
2021-02-04 13:02:56 +0100

    prototypes

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nargouse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/04 12:07:13 by nargouse          #+#    #+#             */
+/*   Updated: 2021/02/04 12:09:43 by nargouse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <limits.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
