3076b3aec81742ead994253b41354cb7c46d47fa
nargouse <nargouse@student.42.fr>
2021-05-12 13:03:31 +0200

    noot noot motherfucker

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nargouse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 12:06:35 by nargouse          #+#    #+#             */
-/*   Updated: 2021/04/19 18:25:22 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/05/11 14:47:40 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	get_next_line(int fd, char **line)
 	char	buff[BUFF_SIZE + 1];
 	int		fd_read;
 
-	if (ifd < 0 || line == NULL || BUFFER_SIZE <= 0)
+	if (fd < 0 || line == NULL || BUFFER_SIZE <= 0)
 		return (-1);
 	while ((fd_read = read(fd, buff, BUFFER_SIZE) > 0)
 	{
